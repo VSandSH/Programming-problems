ec58585a6760398f2c590d702f3df27d7cfaf521
vsandsh <vsandsh@gmail.com>
2014-11-18 23:50:01 +0600

    Create StackTraceCallerMethodName.java
    Every called method in Stack Trace must return the name of the method it was called by.

---
+++ StackTraceCallerMethodName.java
@@ -0,0 +1,44 @@
+public class StackTraceCallerMethodName {
+	public static void main (String[] args) {
+		method1();
+	}
+	
+	public static void method1() {
+		method2();
+		StackTraceElement[] ste = Thread.currentThread().getStackTrace();
+		StackTraceElement e = ste[2];
+		String methodName = e.getMethodName();
+		System.out.println(methodName);
+	}
+	
+	public static void method2() {
+		method3();
+		StackTraceElement[] ste = Thread.currentThread().getStackTrace();
+		StackTraceElement e = ste[2];
+		String methodName = e.getMethodName();
+		System.out.println(methodName);
+	}
+	
+	public static void method3() {
+		method4();
+		StackTraceElement[] ste = Thread.currentThread().getStackTrace();
+		StackTraceElement e = ste[2];
+		String methodName = e.getMethodName();
+		System.out.println(methodName);
+	}
+	
+	public static void method4() {
+		method5();
+		StackTraceElement[] ste = Thread.currentThread().getStackTrace();
+		StackTraceElement e = ste[2];
+		String methodName = e.getMethodName();
+		System.out.println(methodName);
+	}
+	
+	public static void method5() {
+		StackTraceElement[] ste = Thread.currentThread().getStackTrace();
+		StackTraceElement e = ste[2];
+		String methodName = e.getMethodName();
+		System.out.println(methodName);
+	}
+}
